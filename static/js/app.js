047cdd686f48236989634117a559b91af10135e1
Richard Nwokoye <RNwokoye@gmail.com>
2021-05-09 20:45:45 -0400

    update tables

--- static/js/app.js
+++ static/js/app.js
@@ -2,3 +2,19 @@
 var tableData = data;
 
 // YOUR CODE HERE!
+// from data.js
+var tableData = data;
+
+// YOUR CODE HERE!
+var tbody = d3.select("tbody"); 
+
+data.forEach((sighting) => {
+    var row = tbody.append("tr");
+    Object.entries(sighting).forEach(([key, value]) => {
+      var cell = row.append("td");
+      cell.text(value);
+    });
+  });
+
+
+
